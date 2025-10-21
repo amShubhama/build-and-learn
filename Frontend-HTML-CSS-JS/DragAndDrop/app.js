f767ed9387d27bb35a3430d04b6f5c918bd429fb
amShubhama <shubham14sk32@gmail.com>
2025-10-22 02:09:34 +0530

    feat: learn how to implement drag and drop for files

---
+++ Frontend-HTML-CSS-JS/DragAndDrop/app.js
@@ -0,0 +1,23 @@
+const dropArea = document.getElementById('drop-area');
+const inputFile = document.getElementById('input-file');
+const imgView = document.getElementById('img-view');
+
+const uploadImage = () => {
+    const imgLink = URL.createObjectURL(inputFile.files[0]);
+    imgView.style.backgroundImage = `url(${imgLink})`;
+    imgView.textContent = "";
+    imgView.style.border = 0;
+}
+
+inputFile.addEventListener('change', uploadImage);
+
+
+dropArea.addEventListener('dragover', (e) => {
+    e.preventDefault();
+});
+
+dropArea.addEventListener('drop', (e) => {
+    e.preventDefault();
+    inputFile.files = e.dataTransfer.files;
+    uploadImage();
+})
